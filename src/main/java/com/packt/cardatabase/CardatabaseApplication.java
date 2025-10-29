5d132e4fc60006d1d0a70b58d1d203f36bae5878
Your Name <you@example.com>
2025-10-28 23:11:21 -0700

    Using JPA to Create and Access a Database

--- src/main/java/com/packt/cardatabase/CardatabaseApplication.java
+++ src/main/java/com/packt/cardatabase/CardatabaseApplication.java
@@ -1,23 +1,52 @@
 package com.packt.cardatabase;
 
+import java.util.Arrays;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.packt.cardatabase.domain.Car;
+import com.packt.cardatabase.domain.CarRepository;
+import com.packt.cardatabase.domain.Owner;
+import com.packt.cardatabase.domain.OwnerRepository;
+
 @SpringBootApplication
-public class CardatabaseApplication {
+public class CardatabaseApplication implements CommandLineRunner{
 	private static final Logger logger = LoggerFactory.getLogger(
 			CardatabaseApplication.class
-		
 			);
 	
+	private final CarRepository repository;
+	private final OwnerRepository orepository;
+	
+	public CardatabaseApplication(CarRepository repository, OwnerRepository orepository) {
+		this.repository = repository;
+		this.orepository = orepository;
+	}
 
 	public static void main(String[] args) {
-		// After adding this comment the application is restarted.
 		SpringApplication.run(CardatabaseApplication.class, args);
 		logger.info("Applicaion started");
 	}
+	
+	@Override
+	public void run(String... args) throws Exception {
+		Owner owner1 = new Owner("John", "Johnson");
+		Owner owner2 = new Owner("Mary", "Robinson");
+		orepository.saveAll(Arrays.asList(owner1, owner2));
+		
+		repository.save(new Car("Ford", "Mustang", "Red", "ADF-1221", 2023, 59000, owner1));
+		repository.save(new Car("Nissan", "Leaf", "White", "SSJ-1221", 2020, 29000, owner2));
+		repository.save(new Car("Toyota", "Highlander", "Black", "XSE", 2022, 39000, owner2));
 
+	
+	// Fetch all cars and log to console
+	for (Car car : repository.findAll()) {
+		logger.info("brand: {}, model: {}", car.getBrand(), car.getModel());
+	}
+}
+	
 }
