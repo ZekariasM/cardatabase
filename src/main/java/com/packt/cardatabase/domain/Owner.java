652975e6b51ae5a64a13cd76b2ae7d1919ab4202
Your Name <you@example.com>
2025-10-29 05:14:49 -0700

    Creating a RESTful Web Service with Spring Boot

--- src/main/java/com/packt/cardatabase/domain/Owner.java
+++ src/main/java/com/packt/cardatabase/domain/Owner.java
@@ -2,6 +2,9 @@ package com.packt.cardatabase.domain;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -10,11 +13,14 @@ import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 
 @Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer","handler"})
 public class Owner {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long ownerid;
 	private String firstname, lastname;
+	
+	@JsonIgnore
 	@OneToMany(cascade=CascadeType.ALL, mappedBy="owner")
 	private List<Car> cars;
 	
